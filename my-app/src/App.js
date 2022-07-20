bef6e08a820854dd39218f8474a218b56176a95e
Fauzan <heavenzownz@gmail.com>
2022-07-20 16:24:21 +0800

    Install axios

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css'
 import Header from './components/ui/Header'
 
 const App = () => {
+  const [items, setItems] = useState([])
+  const [idLoading, setIsLoading] = useState(true)
+
+  useEffect
   return (
     <div className="container">
       <Header />
