f5860d1d6c8a62b2f4e26f7d30467b0d444d922b
Fauzan <heavenzownz@gmail.com>
2022-07-21 12:12:06 +0800

    Add ternary for CharacterGrid

--- my-app/src/components/characters/CharacterGrid.js
+++ my-app/src/components/characters/CharacterGrid.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-const CharacterGrid = ({}) => {
-  return
+const CharacterGrid = ({ items, isLoading }) => {
+  return isLoading ? <h1>Loading...</h1> : <section className="cards"></section>
 }
 
 export default CharacterGrid
